8b7d29c0f12a2ad67776ace5efc7b258f6143b5b
dennerruby <dennersk891@gmail.com>
2019-11-26 18:55:53 -0300

    datebase config to prodution

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
  
     content_type :json, charset: 'utf-8'
     {
-      "fulfillmentText":response {
+      "fulfillmentText": response {
         "telegram": {
           "text": response,
           "parse_mode": "Markdown"
