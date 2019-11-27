7fbbeb68a844ab92bba4f4367a9473d94286c688
dennerruby <dennersk891@gmail.com>
2019-11-26 22:10:51 -0300

    datebase config to prodution

--- app.rb
+++ app.rb
@@ -1,30 +1,32 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
-  
+
 require './config/database'
-  
+
 # Load Models
 Dir["./app/models/*.rb"].each {|file| require file }
 Dir["./app/services/**/*.rb"].each {|file| require file }
+
+
 class App < Sinatra::Base
   get '/sinatra' do
-    'Hello world!'
+    'Hello world sinatra!'
   end
 
   post '/webhook' do
     request.body.rewind
     result = JSON.parse(request.body.read)["queryResult"]
- 
+
     if result["contexts"].present?
       response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
     else
       response = InterpretService.call(result["action"], result["parameters"])
     end
- 
+
     content_type :json, charset: 'utf-8'
     {
-      "fulfillmentText":{
+      "payload": {
         "telegram": {
           "text": response,
           "parse_mode": "Markdown"
